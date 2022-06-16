1c18164b6663aa084e4bb7671bd1aed56f1ef634
GitEdy2 <clanuzumaki22@gmail.com>
2022-06-15 21:28:16 -0500

    first commit

---
+++ src/components/TimeLine/TimeLine.js
@@ -0,0 +1,107 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { TimeLineData } from '../../constants/constants';
+
+const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
+
+const Timeline = () => {
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselRef = useRef();
+
+  // const scroll = (node, left) => {
+  //   return node.scrollTo({ left, behavior: 'smooth' });
+  // }
+
+  const handleClick = (e, i) => {
+     e.preventDefault();
+
+     if (carouselRef.current) {
+       const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
+      
+       scroll(carouselRef.current, scrollLeft);
+     }
+   } 
+
+  // const handleScroll = () => {
+  //   if (carouselRef.current) {
+  //     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+
+  //     setActiveItem(index);
+  //   }
+  // }
+
+  // // snap back to beginning of scroll when window is resized
+  // // avoids a bug where content is covered up if coming from smaller screen
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     scroll(carouselRef.current, 0);
+  //   }
+
+  //   window.addEventListener('resize', handleResize);
+  // }, []);
+
+  return (
+    <Section id='about'>
+      <SectionTitle>About Me</SectionTitle>
+      <SectionText>
+        Systems Engineer with more than 2 years of experience in web application development using languages such as: .NET,
+        C#, Javascript, React.js, Node.js, MongoDB; and with experience in databases like MySQL and SQL Server. Specializing in
+        Javascript/React.js web development. Technology enthusiast and willing to continue learning. I am interested in getting a
+        job within the Front-End profile for the design and operation of web pages and applications, as well as venturing into the
+        field of mobile development.
+      </SectionText>
+      <CarouselContainer ref={carouselRef}>
+        <>
+          {TimeLineData.map((item, index) => (
+            <CarouselMobileScrollNode key={index} final={index === TOTAL_CAROUSEL_COUNT - 1}>
+              <CarouselItem
+                index={index}
+                id={`carousel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
+              >
+              <CarouselItemTitle>
+                {item.year}
+                <CarouselItemImg
+                    width="208"
+                    height="6"
+                    viewBox="0 0 208 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.5C3.88071 5.5 5 4.38071 5 3V3.5L208 3.50002V2.50002L5 2.5V3C5 1.61929 3.88071 0.5 2.5 0.5C1.11929 0.5 0 1.61929 0 3C0 4.38071 1.11929 5.5 2.5 5.5Z"
+                      fill="url(#paint0_linear)"
+                      fill-opacity="0.33"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear"
+                        x1="-4.30412e-10"
+                        y1="0.5"
+                        x2="208"
+                        y2="0.500295"
+                        gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white" />
+                        <stop
+                          offset="0.79478"
+                          stop-color="white"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                    </defs>
+                  </CarouselItemImg>
+              </CarouselItemTitle>
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          ))}
+        </>
+      </CarouselContainer>
+    </Section>
+  );
+};
+
+export default Timeline;
